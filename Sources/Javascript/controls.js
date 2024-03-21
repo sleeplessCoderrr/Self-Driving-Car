bb273520022624a006aef09d33493db4196e7ab5
Vincentius Oktara <108650831+Vincent1646@users.noreply.github.com>
2024-03-21 22:10:29 +0700

    Initializing the project

---
+++ Sources/Javascript/controls.js
@@ -0,0 +1,29 @@
+class Contols{
+    constructor(){
+        this.forward = false;
+        this.left = false;
+        this.right = false;
+        this.revese = false;
+
+        this.#addKeyboardListeners();
+    }
+
+    #addKeyboardListeners(){
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left = true;
+                    break; 
+                case "ArrowRight":
+                    this.right = true;
+                    break;
+                case "ArrowDown":
+                    this.revese = true;
+                    break;
+                case "ArrowUp":
+                    this.forward = true;
+                    break;
+            }
+        }
+    }
+}
