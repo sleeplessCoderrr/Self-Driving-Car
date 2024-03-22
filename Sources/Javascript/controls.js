bb51a57c8dd11c062b02bce54495375fd432864b
Vincentius Oktara <108650831+Vincent1646@users.noreply.github.com>
2024-03-22 15:11:37 +0700

    fixing bug on movement

--- Sources/Javascript/controls.js
+++ Sources/Javascript/controls.js
@@ -18,7 +18,7 @@ class Controls{
                     this.right = true;
                     break;
                 case "ArrowDown":
-                    this.revese = true;
+                    this.reverse = true;
                     break;
                 case "ArrowUp":
                     this.forward = true;
@@ -30,16 +30,16 @@ class Controls{
         document.onkeyup=(event)=>{
             switch(event.key){
                 case "ArrowLeft":
-                    this.left = true;
+                    this.left = false;
                     break; 
                 case "ArrowRight":
-                    this.right = true;
+                    this.right = false;
                     break;
                 case "ArrowDown":
-                    this.revese = true;
+                    this.reverse = false;
                     break;
                 case "ArrowUp":
-                    this.forward = true;
+                    this.forward = false;
                     break;
             }
             console.table(this);
